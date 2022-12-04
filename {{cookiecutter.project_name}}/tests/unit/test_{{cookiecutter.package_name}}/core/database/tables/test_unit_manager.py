86c29950c687bc2ad77bc345a6776a879698c093
Jordan Zimmitti <jordanzimmitti@gmail.com>
2022-12-04 11:20:06 -0500

    Reformated database-manager unit tests

--- {{cookiecutter.project_name}}/tests/unit/test_{{cookiecutter.package_name}}/core/database/tables/test_unit_manager.py
+++ {{cookiecutter.project_name}}/tests/unit/test_{{cookiecutter.package_name}}/core/database/tables/test_unit_manager.py
@@ -18,48 +18,48 @@ def test_get_connection():
     # Mocks the database-connection class
     database_connection_mock = MagicMock(spec_set=DatabaseConnection)
 
-    # Creates the db-handler instance
-    db_handler = DatabaseManager(
+    # Creates the db_manager instance
+    db_manager = DatabaseManager(
         display_name="test-display-name",
         description="test-description",
         db_uri=SecretStr("test-db-uri"),
     )
-    db_handler._connection = database_connection_mock
+    db_manager._connection = database_connection_mock
 
     # Checks whether the mocked database-connection class was retrieved correctly
-    db_connection = db_handler.connection
+    db_connection = db_manager.connection
     assert db_connection == database_connection_mock
 
 
 def test_get_description():
     """
-    Tests the description property for completion. The description property should return a
-    description
+    Tests the description property for completion. The
+    description property should return a description
     """
 
-    # Creates the db-handler instance
-    db_handler = DatabaseManager(
+    # Creates the db_manager instance
+    db_manager = DatabaseManager(
         display_name="test-name", description="test-description", db_uri=SecretStr("test-db-uri")
     )
 
-    # Checks whether the display-name was retrieved correctly
-    description = db_handler.description
+    # Checks whether the description was retrieved correctly
+    description = db_manager.description
     assert description == "test-description"
 
 
 def test_get_display_name():
     """
-    Tests the display_name property for completion. The display_name property should return a
-    display_name
+    Tests the display_name property for completion. The
+    display_name property should return a display_name
     """
 
-    # Creates the db-handler instance
-    db_handler = DatabaseManager(
+    # Creates the db-manager instance
+    db_manager = DatabaseManager(
         display_name="test-name", description="test-description", db_uri=SecretStr("test-db-uri")
     )
 
     # Checks whether the display-name was retrieved correctly
-    display_name = db_handler.display_name
+    display_name = db_manager.display_name
     assert display_name == "test-name"
 
 
@@ -79,33 +79,33 @@ def test_get_row_operations(mocker):
     database_row_operations_mock = MagicMock(spec_set=DatabaseRowOperations)
     mocker.patch.object(manager, "DatabaseRowOperations", return_value=database_row_operations_mock)
 
-    # Creates the db-handler instance
-    db_handler = DatabaseManager(
+    # Creates the db_manager instance
+    db_manager = DatabaseManager(
         display_name="test-display-name",
         description="test-description",
         db_uri=SecretStr("test-db-uri"),
     )
-    db_handler._connection = database_connection_mock
+    db_manager._connection = database_connection_mock
 
     # Checks whether the mocked database-row-operations class was retrieved correctly
-    db_row_operations = db_handler.row_operations
+    db_row_operations = db_manager.row_operations
     assert db_row_operations == database_row_operations_mock
 
 
 def test_init():
     """
-    Tests the DatabaseHandler init function for completion. The DatabaseHandler init function
-    should instantiate a DatabaseHandler instance without any errors
+    Tests the DatabaseManager init function for completion. The DatabaseManager init
+    function should instantiate a DatabaseManager instance without any errors
     """
 
     # Define and instantiates the database-manager class
-    db_handler = DatabaseManager(
+    db_manager = DatabaseManager(
         display_name="test-display-name",
         description="test-description",
         db_uri=SecretStr("test-db-uri"),
     )
 
     # Checks whether the database-manager class correctly instantiated
-    assert db_handler._display_name == "test-display-name"
-    assert db_handler._description == "test-description"
-    assert db_handler._db_uri == SecretStr("test-db-uri")
+    assert db_manager._display_name == "test-display-name"
+    assert db_manager._description == "test-description"
+    assert db_manager._db_uri == SecretStr("test-db-uri")
